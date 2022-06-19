a4cc8f30be67890d9ded6c238a4b54bc45b63efd
Andres Miranda <andres.miranda.dev@gmail.com>
2022-06-19 23:53:40 +0900

    commiting with better styling, some functions, even though the getnumber function isnt ready,　but ill call it a day for now.

--- main.js
+++ main.js
@@ -26,3 +26,22 @@ function operate (previousOperand, currentOperand, operator){
         return divide(previousOperand, currentOperand);
     };
 };
+
+function getNumber () {
+    const numbers = document.querySelectorAll(".number");
+    const display = document.querySelector("#display")
+    const newValue = [];
+    numbers.forEach((button)=>{
+       button.addEventListener("click", (e)=>{
+         if(display.innerText === "0" && e.target.innerText==="0"){
+            return display.innerText = "0";
+         }else if(display.innerText.length < 21){
+            newValue.push(e.target.innerText);
+            return display.innerText = newValue.join("");
+         }else if(display.innerText === "0" && e.target.innerText === "."){
+             
+         }
+       })
+    })
+}
+getNumber();
